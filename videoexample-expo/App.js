90f705500982c7d267ac9632d40e8c0bbcd3fce4
bdushimi <lespharaons@hotmail.com>
2023-08-28 10:20:51 +0200

    Minor updates

--- videoexample-expo/App.js
+++ videoexample-expo/App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World! Right now it is greeting me</Text>
+      <Text>This is great actually!</Text>
       <StatusBar style="auto" />
     </View>
   );
