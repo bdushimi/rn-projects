eaac6e66f69e5913fe5646d6ab11a67aa80d94d9
bdushimi <lespharaons@hotmail.com>
2024-03-27 00:48:16 +0200

    Adds basic video recording capabilities

--- Instagram/screens/PostUploadScreen/PostUploadScreen.tsx
+++ Instagram/screens/PostUploadScreen/PostUploadScreen.tsx
@@ -1,7 +1,7 @@
 import { MaterialIcons } from "@expo/vector-icons";
 import GlobalStyles from '@theme/GlobalStyles';
 import Colors from '@theme/colors';
-import { Camera, CameraType, FlashMode, CameraPictureOptions } from 'expo-camera';
+import { Camera, CameraType, FlashMode, CameraPictureOptions, CameraRecordingOptions } from 'expo-camera';
 import { useEffect, useState, useRef } from 'react';
 import { Pressable, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
@@ -24,6 +24,7 @@ const PostUploadScreen = () => {
   const [hasPermission, setHasPermission] = useState<boolean | null>(null)
   const [flash, setFlash] = useState(FlashMode.off)
   const [isCameraReady, setIsCameraReady] = useState(false)
+  const [isRecording, setIsRecording] = useState(false)
 
   const cameraRef = useRef<Camera>(null)
 
@@ -63,7 +64,7 @@ const PostUploadScreen = () => {
   }
 
   const onTakePictureHandle = async () => {
-    if (!isCameraReady || !cameraRef.current) {
+    if (!isCameraReady || !cameraRef.current || isRecording) {
       return
     }
     const options: CameraPictureOptions = {
@@ -72,6 +73,34 @@ const PostUploadScreen = () => {
     }
     const result = await cameraRef.current.takePictureAsync(options)
     console.log(result)
+  }
+
+  const onStartRecordingHandle = async () => {
+    if (!isCameraReady || !cameraRef.current || isRecording) {
+      return
+    }
+
+    const videoRecordingOptions: CameraRecordingOptions = {
+      quality: Camera.Constants.VideoQuality['640:480'],
+      maxDuration: 60,
+      maxFileSize: 10 * 1024 * 1024,
+      mute: false
+    }
+
+    try {
+      setIsRecording(true);
+      const video = await cameraRef.current.recordAsync(videoRecordingOptions);
+    } catch (error) {
+      console.error(error);
+    }
+    setIsRecording(false);
+  }
+
+  const onStopRecordingHandle = async () => {
+    if (isRecording) {
+      cameraRef.current?.stopRecording();
+      setIsRecording(false)
+    }
   }
 
 
@@ -95,11 +124,17 @@ const PostUploadScreen = () => {
         </View>
         <View style={[styles.buttonsContainer, { bottom: 25 }]}>
           <MaterialIcons name="photo-library" size={30} color={Colors.white} />
-          {isCameraReady && (
-            <Pressable onPress={onTakePictureHandle}>
-              <View style={styles.circle} />
-            </Pressable>
-          )}
+          {
+            isCameraReady && (
+              <Pressable
+                onPress={onTakePictureHandle}
+                onLongPress={onStartRecordingHandle}
+                onPressOut={onStopRecordingHandle}
+              >
+                <View style={[styles.circle, { backgroundColor: isRecording ? Colors.accent : Colors.white }]} />
+              </Pressable>
+            )
+          }
           <Pressable onPress={onFlipCameraHandle}>
             <MaterialIcons name="flip-camera-ios" size={30} color={Colors.white} />
           </Pressable>
